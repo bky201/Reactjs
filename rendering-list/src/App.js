5d6ad60f379f237528e6b36bef84a17277d13dd2
bek <bekrye@gmail.com>
2023-08-30 23:05:13 +0200

    Add rendering list

---
+++ rendering-list/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import RenderingLists from './components/RenderingLists';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RenderingLists />
+      </div>
+    );
+  }
+}
+
+export default App;
