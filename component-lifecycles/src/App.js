fa2e78489e706bcee43a91e89ba8e55fc6dee0d4
bek <bekrye@gmail.com>
2023-08-31 16:56:29 +0200

    Create Component Did Update

--- component-lifecycles/src/App.js
+++ component-lifecycles/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Clock from './components/Clock';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <Clock /> */}
-        <LifeCyclesCDM />
+        {/* <LifeCyclesCDM /> */}
+        <LifeCyclesCDU />
       </div>
     );
   }
