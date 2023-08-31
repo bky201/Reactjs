e3fc395257828ae5274256fbb79ef4b9a2bcf712
bek <bekrye@gmail.com>
2023-09-01 00:29:24 +0200

    Filter a search

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import UncontrolledForm from './components/UncontrolledForm';
+// import UncontrolledForm from './components/UncontrolledForm';
+// import { SearchBar2 } from "./components/SearchBar2";
+import { SearchBar3 } from "./components/SearchBar3";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UncontrolledForm />
+        {/* <UncontrolledForm /> */}
+        <SearchBar3 />
       </div>
     );
   }
