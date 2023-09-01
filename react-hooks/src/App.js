8003bb11a579e4e0835668fe7424201611563576
bek <bekrye@gmail.com>
2023-09-01 13:02:06 +0200

    Add useState hook with objects and arrays

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 // import ControlledFormHooks from './components/ControlledFormHooks';
-import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <ControlledFormHooks /> */}
-        <UseStateWithArrays />
+        {/* <UseStateWithArrays /> */}
+        <UseStateWithObjects />
       </div>
     );
   }
