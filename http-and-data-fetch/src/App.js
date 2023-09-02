84188f67e55011dac6a139ff66ef603c3d229d31
bek <bekrye@gmail.com>
2023-09-02 23:45:39 +0200

    fetch data using get and Hooks

--- http-and-data-fetch/src/App.js
+++ http-and-data-fetch/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 // import HTTPRequests from './components/HTTPRequests';
-import HTTPPost from './components/HTTPPost';
+// import HTTPPost from './components/HTTPPost';
+import HTTPHooks from './components/HTTPHooks';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <HTTPRequests /> */}
-        <HTTPPost />
+        {/* <HTTPPost /> */}
+        <HTTPHooks />
       </div>
     );
   }
