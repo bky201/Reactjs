76e76cefe09b2f7a1b889d95c257ba6a9546dac8
bek <bekrye@gmail.com>
2023-09-02 23:12:55 +0200

    Send data with post

--- http-and-data-fetch/src/App.js
+++ http-and-data-fetch/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import HTTPRequests from './components/HTTPRequests';
+// import HTTPRequests from './components/HTTPRequests';
+import HTTPPost from './components/HTTPPost';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HTTPRequests />
+        {/* <HTTPRequests /> */}
+        <HTTPPost />
       </div>
     );
   }
